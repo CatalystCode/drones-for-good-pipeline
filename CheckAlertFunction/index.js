41b872be9bdd773903063729f1c83f9bcbed0c41
Elad Iwanir <eladiw@microsoft.com>
2017-07-26 13:47:12 +0300

    more tweaking

--- CheckAlertFunction/index.js
+++ CheckAlertFunction/index.js
@@ -2,25 +2,36 @@
 module.exports = function (context, processedJob) {
     context.log("checking process results for: " + processedJob.url);
     
-    const objectKeywords = [ "man", "woman", "baby", "girl", "boy", "dog", "cat", "pet", "animal_dog", "animal_cat", "plant", "sitting"];
+    const objectKeywords = [ "man", "woman", "baby", "girl", "boy", "dog", "cat", "pet", "animal_dog", "animal_cat", "sitting"];
     const sceneKeywords =  [ "car", "car mirror", "window", "bus", "vehicle" ];
 
-    var findAlertingKeywords = function(tags){
+    var findAlertingKeywords = function(tagsWithConfidence, faces){
             var alertingKeywords = {
                 object: [],
                 scene: []
             };
 
+            // must find a face
+            if (faces.length === 0) {
+                return alertingKeywords;
+            }
+
+            let tagsExtracted = [];
+            tagsWithConfidence.forEach( function(object, objectIndex){ 
+                tagsExtracted.push(object.name);
+            });
+            context.log('tags:' + tagsExtracted);
+
             objectKeywords.forEach( function(object, objectIndex){
-            if(tags.indexOf(object) != -1){
-                alertingKeywords.object.push(object);
-            };
+              if(tagsExtracted.indexOf(object) != -1){
+                  alertingKeywords.object.push(object);
+              }
             });
 
             sceneKeywords.forEach( function(scene, sceneIndex){
-                if(tags.indexOf(scene) != -1){
+                if(tagsExtracted.indexOf(scene) != -1){
                     alertingKeywords.scene.push(scene);
-                };
+                }
             });
 
             return alertingKeywords;
@@ -28,13 +39,13 @@ module.exports = function (context, processedJob) {
 
     
     var cvResult = processedJob.results.cv;
-
-    if(cvResult && cvResult.description && cvResult.description.tags){
-        tags = cvResult.description.tags;
-        var alertingKeywords = findAlertingKeywords(tags);
+    
+    context.log(JSON.stringify(cvResult));
+    if(cvResult && cvResult.faces  && cvResult.tags){
+        var alertingKeywords = findAlertingKeywords(cvResult.tags, cvResult.faces);
 
         // we should alert
-        if (alertingKeywords.object.length > 0 && alertingKeywords.scene.length > 0){
+        if (/* alertingKeywords.object.length > 0 && */ alertingKeywords.scene.length > 0){
             let caption =  cvResult.description.captions && cvResult.description.captions.length > 0 ? cvResult.description.captions[0].text : ""; 
             
             let time = new Date(processedJob.timestamp);
