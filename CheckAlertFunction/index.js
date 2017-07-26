f7341e5d350688c2dc555a4eee0c9b64a58b6f9a
Elad Iwanir <eladiw@microsoft.com>
2017-07-26 12:10:50 +0300

    more tweaking

--- CheckAlertFunction/index.js
+++ CheckAlertFunction/index.js
@@ -2,7 +2,7 @@
 module.exports = function (context, processedJob) {
     context.log("checking process results for: " + processedJob.url);
     
-    const objectKeywords = [ "man", "woman", "baby", "girl", "boy", "dog", "cat", "pet", "animal_dog", "animal_cat", "plant", "tree"];
+    const objectKeywords = [ "man", "woman", "baby", "girl", "boy", "dog", "cat", "pet", "animal_dog", "animal_cat", "plant", "sitting"];
     const sceneKeywords =  [ "car", "car mirror", "window", "bus", "vehicle" ];
 
     var findAlertingKeywords = function(tags){
